2ee828411f77545403fee9d60551107069eac463
LaylaAddi <s.chesnowitz@gmail.com>
2017-03-10 06:40:39 -0500

    set connection to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { Data } from '../providers/data'; //this gets added only to providers array below
+import { AngularFireModule } from 'angularfire2';
+
+ const config = {
+    apiKey: "AIzaSyBhrGRNwel5Hr_emTuJTO4VukSjLaIX0Kc",
+    authDomain: "feed-90035.firebaseapp.com",
+    databaseURL: "https://feed-90035.firebaseio.com",
+    storageBucket: "feed-90035.appspot.com",
+    messagingSenderId: "576527742470"
+  };
+
 
 @NgModule({
   declarations: [
@@ -16,7 +26,8 @@ import { Data } from '../providers/data'; //this gets added only to providers ar
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
